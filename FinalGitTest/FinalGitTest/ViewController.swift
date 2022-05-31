43fe800c95a2788e67a079c3d38400b1ec5d0ace
Renbern <ArthurRenbern@icloud.com>
2022-05-31 20:40:30 +0300

    test

---
+++ FinalGitTest/FinalGitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalGitTest
+//
+//  Created by Артур Щукин on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("test")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
